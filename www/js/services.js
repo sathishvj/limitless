6934ba38ca883121b56ff07b6975d18bb309806a
sathishvj <sathishvj@yahoo.com>
2014-07-05 04:42:37 +0530

    fixed splice bug

--- www/js/services.js
+++ www/js/services.js
@@ -11,7 +11,7 @@ angular.module('starter.services', [])
 		recentsList.unshift(val);
 		for (var i=1; i<recentsList.length; i++) {
 			if (recentsList[i] === val) {
-				recentsList.splice(i, i);	// there's some bug here.  Add 3 items.  Click the same value as the middle item.  The last item is somehow removed.
+				recentsList.splice(i, 1);	// there's some bug here.  Add 3 items.  Click the same value as the middle item.  The last item is somehow removed.
 			}
 		}
 		if (recentsList.length > 10) {
